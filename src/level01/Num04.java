11410144969bb8d95a3d1f61691a3d5e649feab8
Hyesun112 <gpdy0102@gmail.com>
2023-01-04 12:26:24 +0900

    자릿수 더하기

--- src/level01/Num04.java
+++ src/level01/Num04.java
@@ -3,13 +3,19 @@ package level01;
 public class Num04 {
     public int Num04(int n) {
         int answer = 0;
-        /*int num = Integer.valueOf(n);*/
+        String num = String.valueOf(n);
+        System.out.println(num);
+        String[] array = num.split("");
+        for(int i = 0; i < array.length; i++ ){
+            System.out.println(array[i]);
+            answer += Integer.parseInt(array[i]);
+        }
         return answer;
     }
 
     public static void main(String[] args) {
+        int n = 1212;
         Num04 num = new Num04();
-        int n = 123;
         System.out.println(num.Num04(n));
     }
 }
